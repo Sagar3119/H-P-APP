b141f155104ac8fb7ec994b5c467c19dd0345f46
Sagar3119 <80756425+Sagar3119@users.noreply.github.com>
2021-12-23 02:34:39 +0530

    Update Grid and Changes in index.html

--- my-app5/src/app/app-routing.module.ts
+++ my-app5/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataComponent } from './data/data.component';
+import { WizardListComponent } from './wizard-list/wizard-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'wizard-Data', component: DataComponent },
+  { path: 'wizard-list', component: WizardListComponent },
+  { path: '',   redirectTo: '/wizard-list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- my-app5/src/app/app.module.ts
+++ my-app5/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WizardListComponent } from './wizard-list/wizard-list.component';
+import { DataComponent } from './data/data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WizardListComponent
+    WizardListComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
